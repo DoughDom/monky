8e3c6ec1f8b58645de0e2b3c4cec9764ce8f097c
Dominic Survine <dominic.survine@gmail.com>
2023-04-16 12:57:43 -0700

    final commit i guess
    init commit

---
+++ MonkyGame/Assets/Scripts/SnakeTravel.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeTravel : MonoBehaviour
+{
+    public float speed;
+    public bool isExtended;
+    public bool isAttached;
+    public float rotationOffset;
+    public Collider2D PlayerCollider;
+    
+    //public DistanceJoint2D distanceJoint;
+    public LineRenderer lineRenderer;
+    private Vector3 target;
+    //private Rigidbody2D rb;
+    private GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Player");
+        //distanceJoint.enabled = false;
+        isAttached = false;
+        isExtended = false;
+        transform.position = Player.transform.position;
+        target = Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isAttached)
+        {
+            if (transform.position == Player.transform.position)
+            {
+                isExtended = false;
+                lineRenderer.enabled = false;
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                isExtended = true;
+                lineRenderer.enabled = true;
+            }
+
+            else if (!Input.GetMouseButton(0))
+            {
+                target = Player.transform.position;
+            }
+            target.z = transform.position.z;
+        }
+        if (isAttached)
+        {
+            if(!Input.GetMouseButton(0))
+            {
+                isAttached = false;
+            }
+        }
+        
+        lineRenderer.SetPosition(0, Player.transform.position);
+        Vector3 pos = transform.position;
+        pos.z = -0.3f;
+        lineRenderer.SetPosition(1, pos);
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+
+        Vector3 vectorToTarget = Player.transform.position - transform.position;
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - rotationOffset;
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
+    }
+
+
+}

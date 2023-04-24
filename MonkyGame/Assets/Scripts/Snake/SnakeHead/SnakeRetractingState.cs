c70c694d16864ca1cb21a63f1bb03c33e7d707f4
Midfield12 <orangeryan12@gmail.com>
2023-04-24 10:06:26 -0700

    Cleaned Up Code
    -Cleaned up code from snake movement rework

--- MonkyGame/Assets/Scripts/Snake/SnakeHead/SnakeRetractingState.cs
+++ MonkyGame/Assets/Scripts/Snake/SnakeHead/SnakeRetractingState.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class SnakeRetractingState : SnakeState
 {
     private Vector3 target;
-    private Vector3 targetDistance;
     private bool extendBuffer;
 
     public override void EnterState(SnakeController snake)
@@ -11,10 +10,8 @@ public class SnakeRetractingState : SnakeState
         snake.speed = 40f;
         snake.state = SnakeController.State.Retracting;
         target = snake.Player.transform.position;
-        //snake.GetComponent<Rigidbody2D>().AddForce(-(snake.GetComponent<Rigidbody2D>().velocity) * 1f);
         snake.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
         extendBuffer = false;
-        // Debug.Log(snake.GetComponent<Rigidbody2D>().velocity);
         if (snake.GetComponent<Collider2D>().IsTouchingLayers(128))
         {
             snake.SwitchState(snake.idle);
@@ -36,11 +33,8 @@ public class SnakeRetractingState : SnakeState
 
     public override void FixedUpdateState(SnakeController snake)
     {
-        
-        targetDistance = target - snake.transform.position;
         if(snake.transform.position == snake.Player.transform.position)
         {
-            // snake.GetComponent<Rigidbody2D>().AddForce((targetDistance) * -snake.speed, ForceMode2D.Impulse);
             snake.SwitchState(snake.idle);
         }
         else

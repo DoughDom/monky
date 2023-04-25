cdb42f823f20344eb923304215d395400356ef92
Midfield12 <orangeryan12@gmail.com>
2023-04-24 22:39:16 -0700

    Added How To Play In Menu
    - Added a how to play button in main menu that takes you to a quick couple of explanations with pictures on how to play
    Note:
    Images are just a simple representation, will have to add better images later

--- MonkyGame/Assets/Scripts/MenuCanvas/Menu.cs
+++ MonkyGame/Assets/Scripts/MenuCanvas/Menu.cs
@@ -5,16 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-   
+    [SerializeField] private GameObject MainMenu;
+    [SerializeField] private GameObject HowToMenu;
+
+    void Start()
+    {
+        HowToMenu.SetActive(false);
+    }
+    
     public void StartGame()
     {
         Debug.Log("Start");
         SceneManager.LoadScene("Test Course");
     }
 
+    public void HowTo()
+    {
+        Debug.Log("How To Play");
+        MainMenu.SetActive(false);
+        HowToMenu.SetActive(true);
+    }
+
     public void QuitGame()
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    public void Back()
+    {
+        Debug.Log("Back");
+        MainMenu.SetActive(true);
+        HowToMenu.SetActive(false);
     }
 }
